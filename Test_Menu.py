95ffbbe1b962f7b4023c695f536730a507fb4f1c
FriddeFrid91 <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2024-03-26 16:41:28 +0100

    OK

--- Test_Menu.py
+++ Test_Menu.py
@@ -10,7 +10,7 @@ class TestMyMenu(unittest.TestCase):
     def test_show_menu(self):
         """Test for show_menu method."""
         the_menu = Menu()
-        self.assertEqual(the_menu.show_menu(), None)
+        self.assertEquals(the_menu.show_menu(), "Welcome to a game of Pig!\n*************************\n* 1. Player Vs Computer *\n* 2. Player Vs Player   *\n* 3. Rules              *\n* 4. Highscore          *\n* 5. Change name        *\n*************************\n* 6. Exit               *\n*************************")
 
     if __name__ == 'main':
         unittest.main()
