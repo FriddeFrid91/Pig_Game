592dca252643e6d9e3f61c4ec002a511deeeb684
FriddeFrid91 <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2024-03-26 13:14:37 +0100

    Editing the main

--- menu.py
+++ menu.py
@@ -7,37 +7,32 @@ class Menu:
     def __init__(self):
         """Menu constructor."""
         self.menu_options = 0
-    while True:
-        def show_menu(self):
 
-            """Show the menu."""
-            print("_______________________")
-            print("|   >>  Main Menu <<  |")
-            print("|1. Player Vs Computer|")
-            print("|2. Player Vs Player  |")
-            print("|3. Rules             |")
-            print("|4. Highscore         |")
-            print("|---------------------|")
-            print("|5. Exit              |")
-            print("_______________________")
-            
-            self.menu_options = int(input("Select an option: "))
+    def show_menu(self):
+        """Show the menu."""
+        print("Welcome to a game of Pig!")
+        print("*************************")
+        print("* 1. Player Vs Computer *")
+        print("* 2. Player Vs Player   *")
+        print("* 3. Rules              *")
+        print("* 4. Highscore          *")
+        print("*************************")
+        print("* 5. Exit               *")
+        print("*************************")
 
-            if self.menu_options == 1:
-                print("Player Vs Computer")
+    def menu_choices(self, user_choice):
+        """Menu choices."""
+        if user_choice == 1:
+            return "Player Vs Computer"
 
-            elif self.menu_options == 2:
-                print("Player Vs Player")
-                pass
+        elif user_choice == 2:
+            return "Player Vs Player"
 
-            elif self.menu_options == 3:
-                print("Rules")
-                pass
-
-            elif self.menu_options == 4:
-               print("Highscore")
-               pass
-
-            elif self.menu_options == 5:
-               print("Goodbye!")
-               break
+        elif user_choice == 3:
+            return "Rules"
+       
+        elif user_choice == 4:
+            return "Highscore"
+       
+        elif user_choice == 5:
+            return "Goodbye!"
