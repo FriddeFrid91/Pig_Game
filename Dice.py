f7e8be4f94f3ad9f15218ddf907a3f0bcc941870
FriddeFrid91 <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2024-03-28 11:15:56 +0100

    OK

--- Dice.py
+++ Dice.py
@@ -3,30 +3,35 @@ import random
 
 class Dice:
     """Class for the dice."""
-    def __init__(self, numbers): # Constructor
-        roll_the_dice = 0  # Variable to store the result of the dice roll
-        self.numbers = numbers # Number of sides of the dice
+    def __init__(self, numbers):  # Constructor
+        # roll_the_dice = 0  # Variable to store the result of the dice roll
+        self.numbers = numbers  # Number of sides of the dice
 
-    def roll_the_dice(self): # Method to roll the dice
-        return random.randint(1, self.numbers)  # Return a random number between 1 and the number of sides of the dice
-    
-    def showTheDice(self, result, listOfPoints): # Method to show the dice
-        if result == 1: # If the result is 1, the player loses all the points and the turn is over
+    def roll_the_dice(self):  # Method to roll the dice
+        return random.randint(1, self.numbers)  
+    # Return a random number between 1 and the number of sides of the dice
+
+    def showTheDice(self, result, listOfPoints):  # Method to show the dice
+        if result == 1:
+            # If the result is 1, the player loses all the points and the turn is over
             print("--------------------")
             print("Sorry, you got a 1. Your turn is over.")
             print("--------------------")
-                  
-            listOfPoints.clear() # Clear the list of points
+            listOfPoints.clear()  # Clear the list of points
             return 0
-        else: # If the result is not 1, the player gets the points and can continue playing
-            print(f"You got a {result}!") 
+        else:
+            # If the result is not 1, the player gets
+            # the points and can continue playing
+            print(f"You got a {result}!")
             return result
-        
-    def getDice(self): # Getter
+
+    def getDice(self):  # Getter
         return self.numbers
-    
-    def setDice(self, numbers): # Setter
+
+    def setDice(self, numbers):  # Setter
         self.numbers = numbers
-    
-    def __str__(self): # Method to print the dice
-        return f"The dice has {self.numbers} sides." # Return the number of sides of the dice
+
+    def __str__(self):
+        # Method to print the dice
+        return f"The dice has {self.numbers} sides."
+    # Return the number of sides of the dice
