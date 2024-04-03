a72a0fa0ebd1fea58d620c5e99a180b096462697
FriddeFrid91 <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2024-04-03 22:16:26 +0200

    OK

--- Test_player_vs_player.py
+++ Test_player_vs_player.py
@@ -9,9 +9,8 @@ class test_player_vs_player(unittest.TestCase):
     def test_two_player_game(self, mock_input):
         """Test for player_vs_player method."""
         new_game = player_vs_player()
-        result = new_game.two_player_game(mock_input)
+        result = new_game.two_player_game("Player1", "Player2")
         self.assertIsNotNone(result)
-
 
 
 if __name__ == "__main__":
