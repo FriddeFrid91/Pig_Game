c1ec4f6a39d0cfff37906ca70f62363446390eca
FriddeFrid91 <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2024-04-03 22:14:04 +0200

    OK

--- Test_player_vs_player.py
+++ Test_player_vs_player.py
@@ -5,12 +5,13 @@ from unittest.mock import patch
 
 class test_player_vs_player(unittest.TestCase):
     """Test for Player vs Player class."""
-    @patch("builtins.input", side_effect=["Player1"], side_effect2=["Player2"])
+    @patch("builtins.input", side_effect=["Player1", "Player2"])
     def test_two_player_game(self, mock_input):
         """Test for player_vs_player method."""
         new_game = player_vs_player()
-        result = new_game.two_player_game(mock_input)
+        result = new_game.two_player_game(mock_input, mock_input)
         self.assertIsNotNone(result)
+
 
 
 if __name__ == "__main__":
