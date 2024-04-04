6b2c782dfbc69a6f2cd9fb502cc980321c34be07
FriddeFrid91 <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2024-04-04 21:37:59 +0200

    OK

--- Test_highscore.py
+++ Test_highscore.py
@@ -14,9 +14,7 @@ class TestHighscore(unittest.TestCase):
     def test_show_highscore(self):
         """Test the show_highscore function."""
         # Mock the print function
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            self.highscore_instance.show_highscore()
-            self.assertEqual(mock_stdout.getvalue(), "****************\n" + "* HALL OF FAME *\n" + "****************\n")
+        self.assertIsNone(self.highscore_instance.show_highscore())
 
     def test_add_highscore(self):
         """Test the add_highscore function."""
