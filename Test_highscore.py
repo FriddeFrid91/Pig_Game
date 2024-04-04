299b7218a63bdeaec849b17a327f81b0de06613b
FriddeFrid91 <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2024-04-04 21:33:22 +0200

    OK

--- Test_highscore.py
+++ Test_highscore.py
@@ -13,11 +13,10 @@ class TestHighscore(unittest.TestCase):
 
     def test_show_highscore(self):
         """Test the show_highscore function."""
-        # Replace standard output with a StringIO object to capture printed output
+        # Mock the print function
         with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
             self.highscore_instance.show_highscore()
-            expected_output = "****************\n* HALL OF FAME *\n****************\n"
-            self.assertEqual(mock_stdout.getvalue(), expected_output)
+            self.assertEqual(mock_stdout.getvalue(), "HALL OF FAME\n")
 
     def test_add_highscore(self):
         """Test the add_highscore function."""
