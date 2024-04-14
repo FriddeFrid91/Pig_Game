063752fb6fcd750a74b1a43dea949b938dcdc706
Merjam Farj Al-Beibani0015 <merjam.al-beibani0015@stud.hkr.se>
2024-04-14 18:19:39 +0200

    ok

--- Difficulty.py
+++ Difficulty.py
@@ -6,14 +6,37 @@ class Difficulty:
     def __init__(self, difficulty):
         """Initialize the difficulty."""
         self.difficulty = difficulty
+        self.difficulty = 0
+        
+        
+        Difficulty = input("Enter the difficulty: Easy, Medium or Hard?: \n")
+        
+        if Difficulty[0].upper() == "E":
+            d = random.randint(1, 10)
+        elif Difficulty[0].upper() == "M":
+            d = random.randint(1, 50)
+        elif Difficulty[0].upper() == "H":
+             d = random.randint(1, 100)
+        else: 
+            raise Exception("Invalid input, please type a difficulty: \n")
+        self.difficulty = d
+        print(f"Difficulty is set to {d}.")
 
-Difficulty = input("Enter the difficulty: Easy, Medium or Hard?: \n")
-if Difficulty[0].upper() == "E":
-    d = random.randint(1, 10)
-elif Difficulty[0].upper() == "M":
-    d = random.randint(1, 50)
-elif Difficulty[0].upper() == "H":
-    d = random.randint(1, 100)
-else: 
-    raise Exception("Invalid input, please type a difficulty: \n")
-     
+    def get_difficulty(self):
+        """Get the difficulty."""
+        return self.difficulty
+    
+    def set_difficulty(self, difficulty):
+        """Set the difficulty."""
+        self.difficulty = difficulty
+
+if __name__ == "__main__":
+    difficulty = Difficulty()
+    print(difficulty.get_difficulty())
+    difficulty.set_difficulty(50)
+    print(difficulty.get_difficulty())
+    difficulty.set_difficulty(100)
+    print(difficulty.get_difficulty())
+    difficulty.set_difficulty(10)
+    print(difficulty.get_difficulty())  
+    
