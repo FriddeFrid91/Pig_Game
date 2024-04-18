606daa65467e1c2edcda737f92acf1f3f78a3ee9
FriddeFrid91 <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2024-04-18 10:01:53 +0200

    OK

--- Test_dice.py
+++ Test_dice.py
@@ -10,6 +10,15 @@ class TestDice(unittest.TestCase):
         result = dice.roll_the_dice()
         self.assertTrue(1 <= result <= 6)
 
+    def show_the_dice(self):
+        """Test for show_the_dice method."""
+        dice = Dice(6)
+        result = dice.roll_the_dice()
+        if result == 1:
+            self.assertEqual(dice.show_the_dice(result), 0)
+        else:
+            self.assertIsNotNone(dice.show_the_dice(result))
+
 
 if __name__ == "__main__":
     unittest.main()
