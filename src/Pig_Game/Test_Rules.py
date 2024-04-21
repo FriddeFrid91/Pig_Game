b216d374d224335fd8c923559e2c9ce65e613e72
FriddeFrid91 <frida_sanne_amanda.johannesson0060@stud.hkr.se>
2024-04-21 18:44:38 +0200

    OK

--- src/Pig_Game/Test_Rules.py
+++ src/Pig_Game/Test_Rules.py
@@ -10,24 +10,16 @@ class Test_Rules(unittest.TestCase):
 
     def test_show_rules(self):
         """Test show_rules method."""
-        rules = Rules()
-        self.assertEqual(rules.show_rules(),
-                         "The rules are simple: \n"
-                         "1. The game has 2 players, a human and a computer.\n"
-                         "2. Each turn, a player repeatedly rolls a die ",
-                         " until either a 1 is rolled or the player ",
-                         "decides to hold.\n"
-                         "3. If the player rolls a 1, ",
-                         "they score nothing and it becomes ",
-                         "the opponent's turn.\n"
-                         "4. If the player rolls any other number, ",
-                         "it is added to their turn total and the ",
-                         "player's turn continues.\n"
-                         "5. If a player chooses to hold, ",
-                         "their turn total is added to their score, ",
-                         "and it becomes the opponent's turn.\n"
-                         "6. The first player to score ",
-                         "100 or more points wins.\n")
+        the_rules = Rules()
+        self.assertEqual(the_rules.show_rules(), [
+            "\033[95m**RULES OF PIG GAME**\033[0m",
+            "\033[93m********************************************\033[0m",
+            "\033[94mYou can play the game with 1 player with the computer\033[0m",
+            "\033[94mYou can play the game with 2 players\033[0m",
+            "\033[94mFirst player to get 100 points wins\033[0m",
+            "\033[92mIf you roll a 1, you lose all your points for that turn\033[0m",
+            "\033[94mIf you hold, you keep your points for that turn\033[0m",
+            "\033[93m*********************************************\033[0m"])
 
 
 if __name__ == '__main__':
