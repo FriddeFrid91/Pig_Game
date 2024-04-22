fe1d45a05a6766628f623d5ec858c09bf399e857
Merjam Farj Al-Beibani0015 <merjam.al-beibani0015@stud.hkr.se>
2024-04-22 21:04:25 +0200

    new test class

---
+++ src/Pig_Game/Test_player_vs_computer.py
@@ -0,0 +1,17 @@
+"""unit test for player_vs_computer.py"""
+import unittest
+
+from player_vs_computer import player_vs_computer
+"""Test case for Player vs Computer class."""
+class TestPlayerVsComputer(unittest.TestCase):
+    """Test case for Player vs Computer class."""
+    def test_player_vs_computer_game(self):
+        """Test the player_vs_computer_game method."""
+        game = player_vs_computer()
+        game.player_vs_computer_game("easy")
+        game.player_vs_computer_game("medium")
+        game.player_vs_computer_game("hard")
+        self.assertIsNotNone(game)
+        self.assertIsInstance(game, player_vs_computer)
+        # pragma: no cover
+
