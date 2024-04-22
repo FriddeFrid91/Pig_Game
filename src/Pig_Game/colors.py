081aa9eba4d4052b49313f53abbea2f74df23bd7
Merjam Farj Al-Beibani0015 <merjam.al-beibani0015@stud.hkr.se>
2024-04-22 21:49:20 +0200

    yaay finally

--- src/Pig_Game/colors.py
+++ src/Pig_Game/colors.py
@@ -5,10 +5,10 @@ class colors:
     class TextStyles:
         RESET = '\033[0m'      # Reset to default colors and styles
         BOLD = '\033[1m'       # Bold text
-        ITALIC = '\033[3m'     # Italic text (not widely supported)
+        ITALIC = '\033[3m'     # Italic text
         UNDERLINE = '\033[4m'  # Underline text
-        BLINK = '\033[5m'      # Blinking text (not widely supported)
-        REVERSE = '\033[7m'    # Reverse colors (swap foreground and background)
+        BLINK = '\033[5m'      # Blinking text 
+        REVERSE = '\033[7m'    # Reverse colors
         HIDDEN = '\033[8m'
       
 
@@ -18,5 +18,5 @@ class colors:
     BLUE = '\033[94m'
     PINK = '\033[95m'
     CYAN = '\033[96m'
-    RESET = '\033[0m'  # Reset to default colors.
+    RESET = '\033[0m'  #colors.RESET
 
